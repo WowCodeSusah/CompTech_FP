640e919eae5e562fa50bbc92b9b78c0757736222
Michael Bengawan <michaelbengawan1@gmail.com>
2024-12-06 18:55:37 +0700

    fixed parser

--- parser.py
+++ parser.py
@@ -1,10 +1,33 @@
-# Starting Grammar
-# L -> SG
-# L -> NG
-# N - > Noun
-# G -> Adj | Adv
-# G -> VS | VN
-# V -> Verb
+from lark import Lark, tree
 
-def parser(lexical):
-    print('yes')
+grammar = """
+    sentence: NOUN pred obj comp   
+    
+    pred: VERB | 
+    obj: OBJ | ADJ OBJ |
+    comp: ADJ | ADV | 
+
+    NOUN: "he" | "she" | "it"
+    OBJ: "house" | "ball"
+    ADV: "happily"
+    VERB: "eats" | "sits" | "plays"
+    ADJ: "red"
+
+    %import common.WS
+    %ignore WS
+"""
+
+parser = Lark(grammar, start='sentence', ambiguity='explicit')
+
+sentence = 'he eats house'
+
+def make_png(filename):
+    tree.pydot__tree_to_png( parser.parse(sentence), filename)
+
+def make_dot(filename):
+    tree.pydot__tree_to_dot( parser.parse(sentence), filename)
+
+if __name__ == '__main__':
+    print(parser.parse(sentence).pretty())
+    make_png('hello.png')
+    # make_dot(sys.argv[0])
