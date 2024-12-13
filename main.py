b95df0b2ad926bb4d6a7d34f5a0c6513c563ec55
Michael Bengawan <michaelbengawan1@gmail.com>
2024-12-13 10:06:49 +0700

    finished most functionallity

--- main.py
+++ main.py
@@ -1,14 +1,65 @@
 import tkinter as tk
 from tkinter import PhotoImage
+from lark import Lark, tree
+
+grammar = """
+    sentence: NOUN pred obj comp   
+    
+    pred: VERB | 
+    obj: OBJ | ADJ OBJ |
+    comp: ADJ | ADV | 
+
+    NOUN: "he" | "she" | "it"
+    OBJ: "house" | "ball"
+    ADV: "happily"
+    VERB: "eats" | "sits" | "plays"
+    ADJ: "red"
+
+    %import common.WS
+    %ignore WS
+"""
+
+def parser(sentence, pngName ,grammar = grammar):
+    parser = Lark(grammar, start='sentence', ambiguity='explicit')
+
+    def make_png(filename):
+        tree.pydot__tree_to_png( parser.parse(sentence), filename)
+
+    def make_dot(filename):
+        tree.pydot__tree_to_dot( parser.parse(sentence), filename)
+
+    if __name__ == '__main__':
+        print(parser.parse(sentence).pretty())
+        make_png(pngName + '.png')
+        # make_dot(sys.argv[0])
+    
+    return pngName + '.png'
+
 
 # Create main frame
 frame = tk.Tk()
 frame.title("English to Semaphore")
-frame.geometry('1000x500+0+0')
+frame.geometry('1000x700+0+0')
+
+parsed = False
+parseTreeName = ''
 
 def printInput():
-    global count
     inp = inputtxt.get(1.0, "end-1c")
+    global parsed
+    global count
+    global parseTreeName
+
+    if parsed == False:
+        parseTreeName = parser(inp, 'testName')
+        parsed = True
+    else:
+        pass
+
+    parseTreeImage = tk.PhotoImage(file=parseTreeName)
+    parseTree_label.config(image=parseTreeImage)
+    parseTree_label.image = parseTreeImage
+    
     stringList = list(inp)
     if len(stringList) > count:
         lbl.config(text="Provided Input: " + stringList[count])
@@ -16,25 +67,27 @@ def printInput():
         if flagtext == " ":
             image_label.config(image=ready_image)  
         else:
-            try:
-                flag_image = tk.PhotoImage(file="Resouces/" + flagtext + ".png")
-                image_label.config(image=flag_image)
-                image_label.image = flag_image 
-            except Exception as e:
-                lbl.config(text=f"Error loading image for '{flagtext}'")
+                try:
+                    flag_image = tk.PhotoImage(file="Resouces/" + flagtext + ".png")
+                    image_label.config(image=flag_image)
+                    image_label.image = flag_image 
+                except Exception as e:
+                    lbl.config(text=f"Error loading image for '{flagtext}'")
 
+        count = count + 1
         frame.after(1000, printInput)
-        count += 1
     else:
         count = 0
+        parsed = False
+    
 
 # Create a container frame for top-left content
 top_left_frame = tk.Frame(frame)
-top_left_frame.pack(anchor="nw", padx=10, pady=10, expand=True)  # Center vertically
+top_left_frame.pack()  # Center vertically
 
 # Text input
 inputtxt = tk.Text(top_left_frame, height=5, width=50)
-inputtxt.pack(pady=5)
+inputtxt.pack()
 
 count = 0
 
@@ -53,5 +106,8 @@ printButton.pack(pady=5)
 # Label for output text
 lbl = tk.Label(top_left_frame, text="")
 lbl.pack(pady=5)
+
+parseTree_label = tk.Label(top_left_frame, image='')
+parseTree_label.pack(pady=5)
 
 frame.mainloop()
